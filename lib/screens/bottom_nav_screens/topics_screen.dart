e95eb4306b315887e70befc404c07370fda9c314
Zufaa <riyazurrohmankawchar@gmail.com>
2023-05-16 17:50:35 +0600

    4th commit

--- lib/screens/bottom_nav_screens/topics_screen.dart
+++ lib/screens/bottom_nav_screens/topics_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:one_more_wallpaper/model/data_model.dart';
+import 'package:one_more_wallpaper/screens/categories/view_category.dart';
+import 'package:one_more_wallpaper/utils/routes.dart';
 import 'package:one_more_wallpaper/widgets/cutstom_text.dart';
 
 class TopicsScreen extends StatelessWidget {
@@ -18,23 +20,30 @@ class TopicsScreen extends StatelessWidget {
            child: ListView.builder(
                itemCount: topicsList.length,
                itemBuilder: (context,index){
+
+                 final category=topicsList[index]['category'];
+                 final catName=topicsList[index]['catName'];
                  return Padding(
                    padding: const EdgeInsets.all(8.0),
                    child: Stack(
                      children: [
-                       Container(
-                         height: 200,
-                         width: MediaQuery.of(context).size.width,
-                         decoration: BoxDecoration(
-                             color: Colors.blueGrey,
-                           borderRadius: BorderRadius.circular(20),
-                           image: DecorationImage(
-                             image: NetworkImage(topicsList[index]['image_url']),
-                             fit: BoxFit.cover
-                           )
+                       InkWell(
+                         onTap: (){
+                           nextPage(context: context,page: ViewCategory(category:category,catName: catName,));
+                         },
+                         child: Container(
+                           height: 200,
+                           width: MediaQuery.of(context).size.width,
+                           decoration: BoxDecoration(
+                               color: Colors.blueGrey,
+                             borderRadius: BorderRadius.circular(20),
+                             image: DecorationImage(
+                               image: NetworkImage(topicsList[index]['image_url']),
+                               fit: BoxFit.cover
+                             )
+                           ),
                          ),
                        ),
-
                        Positioned(
                          top: 50,
                            left: 20,
